250612757c6eea251a0bb99e0f19f3b0de9ec1b0
asg5450 <asg5450@gmail.com>
2024-03-21 19:18:16 +0900

    컴포넌트에 State 전달하여 기능 구현
    컴포넌트에 State 전달하여 기능 구현

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import Viewer from "./component/Viewer.js";
 import Controller from "./component/Controller.js";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller handleSetCount={handleSetCount} />
       </section>
     </div>
   );
